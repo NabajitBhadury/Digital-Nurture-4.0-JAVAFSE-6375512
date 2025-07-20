e5e24a98920efe19e3cfed6ae12ab3d8499b2814
NabajitBhadury <nabajitbhadury@gmail.com>
2025-07-20 13:19:26 +0530

    week 5 accomplished

---
+++ week5-assesment/eid-1/microservices/account-loan/src/main/java/com/cognizant/account/loan/AccountLoanApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.account.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountLoanApplication.class, args);
+	}
+
+}
---
+++ week5-assesment/eid-1/microservices/account-loan/src/test/java/com/cognizant/account/loan/AccountLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.account.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
